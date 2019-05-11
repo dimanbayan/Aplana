41d52b5e3cdbb894da5d0de8372bcf78eda797ec
dimanbayan <dmitr_555@mail.ru>
2019-05-12 00:44:50 +0300

    Задача №8: Цикличный вывод данных на экран

---
+++ src/part2/From1to50.java
@@ -0,0 +1,13 @@
+package part2;
+
+/*
+Задача №8: Цикличный вывод данных на экран
+ */
+
+public class From1to50 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 51; i++) {
+            System.out.println(i);
+        }
+    }
+}
