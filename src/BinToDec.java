91d1cd898d0b2b88448daf91c343c1ba0e67980d
dimanbayan <dmitr_555@mail.ru>
2019-05-11 22:38:36 +0300

    Задача №4: Отработка поразрядных операций

---
+++ src/BinToDec.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BinToDec {
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите число в двоичной системе счисления:");
+        String binary = r.readLine();
+
+        int decimal = Integer.parseInt(binary, 2);
+
+        System.out.println("Число в десятичной системе счисления имеет значение: \n" + decimal);
+    }
+}
